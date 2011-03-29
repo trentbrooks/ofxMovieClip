a3d9676c670ffba7e2658665dccc6df1ab66fa9b
Trent Brooks <trentbrooks@gmail.com>
2011-03-30 00:34:37 +1300

    fixed dynamic frame rate

--- ofxFBOMovieClip/ofxFBOMovieClip.cpp
+++ ofxFBOMovieClip/ofxFBOMovieClip.cpp
@@ -6,6 +6,7 @@ ofxFBOMovieClip::ofxFBOMovieClip(){
 	reversePlayheadCount = 0;
 	playMode = 1;
 	frameRate = 30.0f;
+	frameRateCount = 0;
 	frameLabelId = 0;
 }
 
@@ -20,21 +21,42 @@ void ofxFBOMovieClip::init(ofxFBOImageSequenceLoader* fboIs, float fRate)
 void ofxFBOMovieClip::setFrameRate(float fr)
 {
 	userSelectFrameRate = fr;
-	frameRate = (ofGetFrameRate() / userSelectFrameRate) * ofGetFrameRate();
+	//frameRate = (ofGetFrameRate() / userSelectFrameRate) * ofGetFrameRate();
+	
+	// framerate cannot be higher that app or lower than 1.
+	frameRate = ofClamp(ofGetFrameRate() - userSelectFrameRate, 1.0f, ofGetFrameRate()); 
 }
 
 
 //--------------------------------------------------------------
 void ofxFBOMovieClip::stepForward()
 {
-	playheadCount = (playheadCount + 1) % (int)frameRate; 
+	
+	//playheadCount = (playheadCount + 1) % (int)frameRate; 
+	
+	frameRateCount = (frameRateCount + 1) % (int)frameRate;
+	if(frameRateCount == (int)frameRate - 1)
+	{
+		playheadCount = (playheadCount + 1) % (int)fboImageSequence->assetCollections[frameLabelId].size();
+	}
+	
+	
+
+
 }
 
 //--------------------------------------------------------------
 void ofxFBOMovieClip::stepReverse()
 {
-	reversePlayheadCount = (reversePlayheadCount + 1) % (int)frameRate;
-	playheadCount = frameRate - reversePlayheadCount - 1;
+	//reversePlayheadCount = (reversePlayheadCount + 1) % (int)frameRate;
+	//playheadCount = frameRate - reversePlayheadCount - 1;
+	
+	frameRateCount = (frameRateCount + 1) % (int)frameRate;
+	if(frameRateCount == (int)frameRate - 1)
+	{
+		reversePlayheadCount = (reversePlayheadCount + 1) % (int)fboImageSequence->assetCollections[frameLabelId].size();
+		playheadCount = (int)fboImageSequence->assetCollections[frameLabelId].size() - reversePlayheadCount - 1;
+	}
 }
 
 
@@ -43,20 +65,24 @@ void ofxFBOMovieClip::stepReverse()
 void ofxFBOMovieClip::play()
 {
 	playMode = STEP_FORWARD;
+	frameRateCount = 0;
 }
 
 //--------------------------------------------------------------
 void ofxFBOMovieClip::reverse()
 {
 	// need to reswap reversePlayheadCount if playing
-	reversePlayheadCount = frameRate - playheadCount;
+	//reversePlayheadCount = frameRate - playheadCount;
+	reversePlayheadCount = (int)fboImageSequence->assetCollections[frameLabelId].size() - playheadCount;
 	playMode = STEP_REVERSE;
+	frameRateCount = 0;
 }
 
 //--------------------------------------------------------------
 void ofxFBOMovieClip::stop()
 {
 	playMode = STEP_STOP;
+	frameRateCount = 0;
 }
 
 //--------------------------------------------------------------
@@ -64,6 +90,7 @@ void ofxFBOMovieClip::restart()
 {
 	playMode = STEP_FORWARD;
 	playheadCount = 0;
+	frameRateCount = 0;
 }
 
 //--------------------------------------------------------------
@@ -71,6 +98,7 @@ void ofxFBOMovieClip::gotoAndPlay(int frameNumber)
 {
 	playMode = STEP_FORWARD;
 	playheadCount = (ofClamp(float(frameNumber-1), 0.0f,fboImageSequence->assetCollections[frameLabelId].size()) / fboImageSequence->assetCollections[frameLabelId].size()) * frameRate;
+	frameRateCount = 0;
 }
 
 //--------------------------------------------------------------
@@ -78,6 +106,7 @@ void ofxFBOMovieClip::gotoAndStop(int frameNumber)
 {
 	playMode = STEP_STOP;
 	playheadCount = (ofClamp(float(frameNumber), 0.0f, fboImageSequence->assetCollections[frameLabelId].size()) / fboImageSequence->assetCollections[frameLabelId].size()) * frameRate;
+	frameRateCount = 0;
 }
 
 //--------------------------------------------------------------
@@ -86,6 +115,7 @@ void ofxFBOMovieClip::gotoAndPlay(string frameLabel)
 	playMode = STEP_FORWARD;
 	frameLabelId = fboImageSequence->getAssetsId(frameLabel);
 	playheadCount = 0;
+	frameRateCount = 0;
 }
 
 //--------------------------------------------------------------
@@ -94,6 +124,7 @@ void ofxFBOMovieClip::gotoAndStop(string frameLabel)
 	playMode = STEP_STOP;
 	frameLabelId = fboImageSequence->getAssetsId(frameLabel);
 	playheadCount = 0;		
+	frameRateCount = 0;
 }
 
 //--------------------------------------------------------------
@@ -146,13 +177,15 @@ void ofxFBOMovieClip::drawFrame(int x, int y, float w, float h)
 ofxFBOTexture* ofxFBOMovieClip::getFrame()
 {
 	tick(); // now gets called whenever a getFrame is requested instead of manually	
-	return getFrameAtPercent(playheadCount / frameRate); // what if playhead is way more than frame rate dick?
-	//return getFrameAtPercent(playheadCount / float(fboImageSequence->assetCollections[frameLabelId].size()));
+	//return getFrameAtPercent(playheadCount / frameRate); // what if playhead is way more than frame rate dick?
+	return getFrameAtPercent(playheadCount / float(fboImageSequence->assetCollections[frameLabelId].size()));
 }
 
 //--------------------------------------------------------------
 ofxFBOTexture* ofxFBOMovieClip::getFrameAtPercent(float percent)
 {
+	//printf("mc percent: %f ", percent); //, playheadCount, float(fboImageSequence->assetCollections[frameLabelId].size()) );
+	//printf("mc playhead: %i \n", playheadCount);
 	return fboImageSequence->assetCollections[frameLabelId][(int)(percent * fboImageSequence->assetCollections[frameLabelId].size())];
 }
 
--- ofxFBOMovieClip/ofxFBOMovieClip.h
+++ ofxFBOMovieClip/ofxFBOMovieClip.h
@@ -3,40 +3,15 @@
  *
  * Created by Trent Brooks, http://www.trentbrooks.com
  *
- * Permission is hereby granted, free of charge, to any person
- * obtaining a copy of this software and associated documentation
- * files (the "Software"), to deal in the Software without
- * restriction, including without limitation the rights to use,
- * copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the
- * Software is furnished to do so, subject to the following
- * conditions:
- *
- * The above copyright notice and this permission notice shall be
- * included in all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
- * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
- * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
- * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
- * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
- * OTHER DEALINGS IN THE SOFTWARE.
- *
  * ----------------------
  *
  *	ABOUT
  *  ofxFBOMovieClip & ofxFBOImageSequenceLoader are a set of helper 
  *  classes for loading and controlling image sequences as FBO textures. 
- *  These were created to add simple timeline control similar to Flash.  
+ *  These were created to add simple timeline control similar to Flash  
  *  ( play(), stop(), gotoAndPlay(), etc ). ofxFBOImageSequenceLoader 
  *  loads sequenced images, and ofxFBOMovieClip requires a pointer
  *  to ofxFBOImageSequenceLoader to control the playback of the frames.
- *
- *
- *  SAMPLE USAGE
- * 
  *
  *  ofxFBOImageSequenceLoader is similar to and inspired by 
  *  FlightPhase's http://www.flightphase.com ofxImageSequence.
@@ -110,6 +85,7 @@ class ofxFBOMovieClip
 		int frameLabelId;		
 		int playheadCount; 
 		int reversePlayheadCount;
+		int frameRateCount;
 		int playMode; // 0 = stopped, 1 = playing, 2 = reverse
 		
 		
